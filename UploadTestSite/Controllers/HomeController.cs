c07728044854e5842c9f8fbfa2a005c6fe8b5ca7
Tom Needham <06needhamt@gmail.com>
2017-10-18 14:36:54 +0100

    Add Blank Uploader View

--- UploadTestSite/Controllers/HomeController.cs
+++ UploadTestSite/Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace UploadTestSite.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Uploader()
+        {
+            return View();
+        }
     }
 }
